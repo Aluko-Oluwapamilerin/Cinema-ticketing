21e9be257a6cda450f4918ff72de65c13bdc3eba
oluwapamilerin <alukooluwapamilerin1@gmail.com>
2022-03-15 16:40:48 +0100

    added a new feature

---
+++ template/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDL4BqOZa0Vipb_0OhuA9uUeynB_647oBE",
+  authDomain: "palms-cinema-457df.firebaseapp.com",
+  databaseURL: "https://palms-cinema-457df-default-rtdb.firebaseio.com",
+  projectId: "palms-cinema-457df",
+  storageBucket: "palms-cinema-457df.appspot.com",
+  messagingSenderId: "339389616366",
+  appId: "1:339389616366:web:e957e2de7007b83d0be3ff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
